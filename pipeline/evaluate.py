791ef3e93121e3641b44670fda4285bed3e7a9fc
ERRAMI  Hafssa <127532160+Diyoukmaine@users.noreply.github.com>
2025-12-06 11:50:10 +0100

    Fix indentation and add newline at end of file

--- pipeline/evaluate.py
+++ pipeline/evaluate.py
@@ -2,7 +2,6 @@ from core.loss import compute_log_loss
 from sklearn.metrics import accuracy_score
 
 
-
 def evaluate(model, X_test, y_test):
     y_pred = model.predict(X_test)
     y_pred_proba = model.model.predict_proba(X_test)[:, 1]
